dbd0fb45220a714fb5015f057cf82589b4718f48
khoryongteng <yongtengkhor338@gmail.com>
2023-07-04 09:03:57 +0100

    Ran Training for 170 Epochs

--- train.py
+++ train.py
@@ -1,15 +1,21 @@
 from ultralytics import YOLO
 import os 
 
-path = os.path.dirname(os.path.abspath(__file__))
-# Load the model.
-model = YOLO('yolov8n.pt')
- 
-# Training.
-results = model.train(
-   data= path + "/datasets/data.yaml",
-   imgsz=416,
-   epochs=10,
-   batch=8,
-   name='yolov8n_custom'
-)
+def train():
+   path = os.path.dirname(os.path.abspath(__file__))
+   # Load the model.
+   model = YOLO('yolov8n.pt')
+   
+   # Training.
+   model.train(
+      data= path + "/datasets/data.yaml",
+      imgsz=416,
+      epochs=300,
+      batch=8,
+      name='yolov8n_custom',
+      save=True,
+      save_period = 10
+   )
+
+if __name__ == '__main__':
+   train()
